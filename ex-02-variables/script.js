44bd462e936c1678c5c40999d59e7bd8f407f4d8
meddlin <drushing.dev@gmail.com>
2022-02-04 02:12:44 -0600

    Exercise 02 - Variables

--- ex-02-variables/script.js
+++ ex-02-variables/script.js
@@ -1,19 +1,27 @@
-
-// We can store data in a special label called a "variable"
+/**
+ * We can store data in a special label called a "variable"
+ */
 var myString = "Some text";
 var myNum = 3;
 
-// We can use those variables later! Here, we print them out.
+
+/**
+ * We can use those variables later! Here, we print them out.
+ */
 console.log("Look! We can print stuff!");
 console.log(myString);
 console.log(myNum);
 
 
-// We can capture values in variables
+/**
+ * We can capture values in variables
+ */
 var sum = myNum + 5;
 console.log(sum);
 
 
-// We can do weird things, like smash numbers and strings together!
-var weirdResult = myString + sum;
+/**
+ * We can do weird things, like smash numbers and strings together!
+ */
+var weirdResult = myString + myNum;
 console.log(weirdResult);
