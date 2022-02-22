2e713f6b65b085dd065be1a270bbe023010fe1a1
meddlin <drushing.dev@gmail.com>
2022-02-21 19:30:36 -0600

    Adding the flow control examples

--- ex-03-flow-control/script.js
+++ ex-03-flow-control/script.js
@@ -1,79 +1,49 @@
-// Exercise 3 - If-conditions
-//
-//
-
-// /**
-//  * Introduction
-//  */
-// // Let's introduce a new type of variable--boolean
-// var isSkyBlue = true;
-// var isVanillaSpicy = false;
-
-// // Let's print one out to see what they do
-// console.log("Is the sky blue?" + isSkyBlue);
-
-
-
-
-
-/**
- * How to use it
- * Some explanation and negation
+/** 
+ * Exercise 3 - Flow control
+ * 
+ * "Flow control" (program flow) is how we can give our
+ *  code some specific instructions about what to execute next.
  */
-// Introducing: if-conditions, this is a way to make decisions in JavaScript
-if (isSkyBlue) {
-    console.log('The sky is blue!');
+
+/*************************
+ *  If-statements
+ *************************/
+ var isSkyBlue = true; // We set a condition...something resolves to true or false
+
+ // if the condition is true, then we execute our code! ...if not, then it doesn't execute
+ if (isSkyBlue) {
+     console.log('DAY time!');
+ }
+ 
+ // we can also program for something to happen if the condition isn't true
+ if (isSkyBlue) {
+     console.log('DAY time!');
+ } else {
+     console.log('NIGHT time');
+ }
+
+
+
+ /*************************
+ * While loops
+ *************************/
+
+// Let's keep our same condition...
+var x = 0;
+while(x < 5) {
+    console.log('...time passes'); // **while** our condition is true, our code will execute
+    x = x + 1;
 }
 
-// Note: the if-condition executes when its condition is 'true'
-if (!isVanillaSpicy) {
-    console.log("Of course it isn't...it's ice cream!");
+
+
+/**************************
+ * Bonus: Combining for-loop and if-statement
+ *************************/
+for (var i = 0; i <= 10; i++) {
+
+    if (i % 2 == 0) {
+        console.log(`EVEN number - ${i}`);
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /**
-//  * if-else
-//  * We can also connect decisions (it doesn't matter if they make sense)
-//  */
-// if (!isSkyBlue) {
-//     console.log("Of course, it's blue!");
-// } else {
-//     console.log("Then it's night time!");
-// }
-
-// // or...
-
-// if (!isSkyBlue) {
-//     console.log("Of course, it's blue!");
-// } else if (isVanillaSpicy) {
-//     console.log("Then it's night time!");
-// } else {
-//     console.log('Did you expect us to fall down here?')
-// }
-
-
-// /**
-//  * Comparison & Logical Operators 
-//  * ...w/ if, if-else
-//  */
-// if (3 > 2) {
-//     console.log('Arithmetic still works!');
-// }
-
-// if (isSkyBlue && 1 < 7) {
-//     console.log("Okay, we're starting to combine things ")
-// }
