60440902b5ee6a7cac9b89832a2b8d83fa6751b6
meddlin <drushing.dev@gmail.com>
2022-02-21 20:26:06 -0600

    Cleaned up old structure
    - Renamed some folders
    - Removed old versions of exercise 1 and 2

--- exercise-02/script.js
+++
@@ -1,8 +0,0 @@
-var myVariable = 'Hey, this is some data';
-var three = 3;
-var five = 5;
-
-console.log(myVariable);
-
-console.log( 'Answer: ' + (three - five) );
-// console.log( 'Answer: ' + -2 );
